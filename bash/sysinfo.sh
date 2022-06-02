7564fe0a94ab1491c2e4bd721eb551f26603ead4
ajay200463122 <105761437+ajay200463122@users.noreply.github.com>
2022-06-01 23:58:56 -0400

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 echo Identity information of my Computer
-exit
+hostname = ubuntu
+linux distro = Linux ubuntu 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux
+ip address = 192.168.193.132
+amount of space available in the root filesystem = Filesystem      Size  Used Avail Use% Mounted on
+udev            985M     0  985M   0% /dev
+tmpfs           204M  1.7M  202M   1% /run
+/dev/sda5        21G  9.0G   11G  47% /
+tmpfs           1.1G     0  1.1G   0% /dev/shm
+tmpfs           5.3M  4.1k  5.3M   1% /run/lock
+tmpfs           1.1G     0  1.1G   0% /sys/fs/cgroup
+/dev/loop0       59M   59M     0 100% /snap/core18/1988
+/dev/loop1       59M   59M     0 100% /snap/core18/2409
+/dev/loop2       66M   66M     0 100% /snap/core20/1434
+/dev/loop3      132k  132k     0 100% /snap/bare/5
+/dev/loop4      230M  230M     0 100% /snap/gnome-3-34-1804/66
+/dev/loop5       69M   69M     0 100% /snap/gtk-common-themes/1519
+/dev/loop6       69M   69M     0 100% /snap/gtk-common-themes/1514
+/dev/loop7      261M  261M     0 100% /snap/gnome-3-38-2004/99
+/dev/loop8      230M  230M     0 100% /snap/gnome-3-34-1804/77
+/dev/loop9       57M   57M     0 100% /snap/snap-store/558
+/dev/loop10      54M   54M     0 100% /snap/snap-store/518
+/dev/loop11      47M   47M     0 100% /snap/snapd/15534
+/dev/sda1       536M  4.1k  536M   1% /boot/efi
+tmpfs           204M   78k  204M   1% /run/user/1000
+
